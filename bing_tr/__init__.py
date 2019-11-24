916f6d24261fc56e46afddbd748963577f745044
ffreemt <yucongo+fmt@gmail.com>
2019-11-24 16:52:51 +0800

    fixed __main__.py, hopefully

--- bing_tr/__init__.py
+++ bing_tr/__init__.py
@@ -1,5 +1,5 @@
 '''bing translate for free, as in beer'''
-from .bing_tr import bing_tr
+from .bing_tr import bing_tr, LANG_CODES
 
 __version__ = '0.0.3'
 __date__ = '2019.11.23'
--- bing_tr/__main__.py
+++ bing_tr/__main__.py
@@ -4,25 +4,36 @@ python -m bing_tr
 import sys
 from random import randint
 
-from bing_tr import bing_tr
+from bing_tr import bing_tr, LANG_CODES
 
 
 # pragma: no cover
 def main():
     '''main'''
-    if not sys.argv[3:]:
-        to_lang = 'zh'
 
-    if not sys.argv[2:]:
-        from_lang = 'en'
+    from_lang = 'auto'
+    to_lang = 'zh'
 
     if not sys.argv[1:]:
-        print('Provide some English text, testing with some random text')
+        print('Provide some English text, with an optional to_lang')
+        print('E.g., python -m bing_tr test this and that de')
+        print('Testing with some random text')
         text = 'test ' + str(randint(0, 10000))
+    else:
+        argv = sys.argv[1:]
+        len_ = len(argv)
+
+        if len_ == 1:
+            text = argv
+        elif argv[-1] in LANG_CODES:
+            to_lang = argv[-1]
+            text = ' '.join(argv[:-1])
+        else:
+            text = ' '.join(argv)
 
     resu = bing_tr(text, from_lang, to_lang)
 
-    print(f'[{text}] translated to: [{resu}]')
+    print(f'[{text}] translated to [{to_lang}]: [{resu}]')
 
 
 if __name__ == '__main__':
